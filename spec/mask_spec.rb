6594ff9eeea87f703f210fb0e2279a49774926e0
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2013-01-13 16:41:32 +0400

    http://en.wikipedia.org/wiki/Steganography

---
+++ spec/mask_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+sample("Steganography_original.png") do |fname|
+  describe fname do
+    it "extracts hidden image" do
+      tname = "tmp/mask.tmp.png"
+      File.unlink(tname) if File.exist?(tname)
+      cli(ZSteg::MaskCLI, fname, "-m 00000011 -O #{tname}")
+      img1 = ZPNG::Image.load tname
+      img2 = ZPNG::Image.load fname.sub(/\.png$/,".00000011.png")
+      img1.should == img2
+    end
+  end
+end
