b6f426aaf959c04fae5eaad874f59fe87ccf29bf
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2013-01-20 03:20:22 +0400

    http://www.newbiecontest.org/index.php?page=epreuve&no=225

---
+++ spec/newbiecontest_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+# scanline extradata
+sample("newbiecontest/alph1-surprise.bmp") do |fname|
+  describe fname do
+    subject{ cli(fname) }
+
+    it { should include "PE32 executable for MS Windows" }
+    it { should include "is program canno" }
+
+    describe "--extract" do
+      subject{ cli(fname, "--extract scanline") }
+
+      it { should include "MessageBoxA" }
+      it { should include "PVUAC PY HCHY UCYL AOPCISV WJHXY JDVYZJI YXH NIDSYRFBRCASVMFWVY" }
+    end
+  end
+end
